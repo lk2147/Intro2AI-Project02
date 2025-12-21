8f05ab0ed08b3ce04f8f3b49cccff6aebd408a3d
hotrhoth <hotrhoth@gmail..com>
2025-12-21 22:15:17 +0700

    update autorun

--- Source/auto-run.py
+++ Source/auto-run.py
@@ -3,7 +3,5 @@ algos = ['astar', 'pysat', 'backtracking', 'bruteforce']
 for algo in algos:
     for size in [7, 9, 11, 13, 17, 20]:
         for i in range(1, 6):
-            if (size > 9):
-                print(f"Skipping {algo} for size {size}x{size}")
             res = os.popen(f"python main.py -a {algo} -f Inputs/{size}x{size}/input-0{i}.txt").read()
             print(res)
